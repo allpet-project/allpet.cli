f85ea64c52094b8f8915b560328f1431548d3b83
lights <lightsever@hotmail.com>
2019-01-03 19:58:08 +0800

    add view

---
+++ nodecli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nodecli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
