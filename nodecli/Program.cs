8cbdf43826d55efe59b14064a424c5d1e3416bc6
lights <lightsever@hotmail.com>
2019-01-08 15:35:28 +0800

    add sample

--- nodecli/Program.cs
+++ nodecli/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 
-namespace nodecli
+namespace allpet.nodecli
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //把当前目录搞对，怎么启动都能找到dll了
+            var lastpath = System.IO.Path.GetDirectoryName(typeof(Program).Assembly.Location); ;
+            Console.WriteLine("exepath=" + lastpath);
+            Environment.CurrentDirectory = lastpath;
+
+
+            var configstr = System.IO.File.ReadAllText("config.json");
+            var config = Config.Parse(configstr);
+
+            InitMenu();
+            MenuLoop();
         }
+        static void InitMenu()
+        {
+            AddMenu("exit", "exit application", (words) => { Environment.Exit(0); });
+            AddMenu("help", "show help", ShowMenu);
+        }
+        #region MenuSystem
+        static System.Collections.Generic.Dictionary<string, Action<string[]>> menuItem = new System.Collections.Generic.Dictionary<string, Action<string[]>>();
+        static System.Collections.Generic.Dictionary<string, string> menuDesc = new System.Collections.Generic.Dictionary<string, string>();
+
+        static void AddMenu(string cmd, string desc, Action<string[]> onMenu)
+        {
+            menuItem[cmd.ToLower()] = onMenu;
+            menuDesc[cmd.ToLower()] = desc;
+        }
+
+        static void ShowMenu(string[] words = null)
+        {
+            Console.WriteLine("== NodeCLI Menu==");
+            foreach (var key in menuItem.Keys)
+            {
+                var line = "  " + key + " - ";
+                if (menuDesc.ContainsKey(key))
+                    line += menuDesc[key];
+                Console.WriteLine(line);
+            }
+        }
+        static void MenuLoop()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("-->");
+                    var line = Console.ReadLine();
+                    var words = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    if (words.Length > 0)
+                    {
+                        var cmd = words[0].ToLower();
+                        if (cmd == "?")
+                        {
+                            ShowMenu();
+                        }
+                        else if (menuItem.ContainsKey(cmd))
+                        {
+                            menuItem[cmd](words);
+                        }
+                    }
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine("err:" + err.Message);
+                }
+            }
+        }
+        #endregion
     }
 }
---
+++ nodecli/config/config.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace allpet.nodecli
+{
+    class Config
+    {
+        class ChainInfo
+        {
+            public string MagicStr;
+            public string[] InitOwner;
+            public byte[] ToInitScript()
+            {
+                return null;
+            }
+        }
+        ChainInfo chainInfo = new ChainInfo();
+
+        static string[] GetStringArrayFromJson(JArray jarray)
+        {
+            var strs = new string[jarray.Count];
+            for (var i = 0; i < jarray.Count; i++)
+            {
+                strs[i] = (string)jarray[i];
+            }
+            return strs;
+        }
+        private Config()
+        {
+
+        }
+        public static Config Parse(string txt)
+        {
+            var jobj = JObject.Parse(txt);
+            Config c = new Config();
+            c.chainInfo.MagicStr = (string)jobj["ChainInfo"]["MagicStr"];
+            c.chainInfo.InitOwner = GetStringArrayFromJson((JArray)jobj["ChainInfo"]["InitOwner"]);
+            return c;
+        }
+    }
+}
