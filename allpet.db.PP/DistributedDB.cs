bb9c6bee4e438f572910f41a1c5c8ef9707ca69f
dongbo <895188445@qq.com>
2019-01-23 17:57:08 +0800

    simpledb + pipeline

---
+++ allpet.db.PP/DistributedDB.cs
@@ -0,0 +1,50 @@
+﻿using AllPet.db.simple;
+using AllPet.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace allpet.db.PP
+{
+    public class DistributedDB : Pipeline
+    {
+        DB db = new DB();
+        Dictionary<ActionEnum, BaseAction> actionFactory = new Dictionary<ActionEnum, BaseAction>();
+
+        public DistributedDB(IPipelineSystem system) : base(system)
+        {
+            this.registeAction(ActionEnum.Put,new PutAction(db));
+        }
+        public override void OnTell(IPipelineRef from, byte[] data)
+        {
+            ActionEnum actiontype= (ActionEnum)StreamHelp.readByte(data);
+            if(this.actionFactory.ContainsKey(actiontype))
+            {
+                this.actionFactory[actiontype].handle(from,data);
+            }
+        }
+
+        void registeAction(ActionEnum action, BaseAction actionInc)
+        {
+            this.actionFactory.Add(action, actionInc);
+        }
+    }
+
+    public enum ActionEnum
+    {
+        Put,
+        Delete,
+        CreateTable,
+        DeleteTable
+    }
+
+    public abstract class BaseAction
+    {
+        protected DB db;
+        public BaseAction(DB dB)
+        {
+            this.db = dB;
+        }
+        public abstract void handle(IPipelineRef from, byte[] data);
+    }
+}
---
+++ allpet.db.PP/nodeModule.cs
@@ -0,0 +1,39 @@
+﻿using AllPet;
+using AllPet.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace allpet.db.PP
+{
+    public class NodeModule : Pipeline
+    {
+        Dictionary<string, IPipelineRef> DataServerDic = new Dictionary<string, IPipelineRef>();
+        List<string> serverPath = new List<string>();
+
+        public NodeModule(IPipelineSystem system) : base(system)
+        {
+
+        }
+
+        public override void OnTell(IPipelineRef from, byte[] data)
+        {
+            var database = getServer(data);
+            database.Tell(data);
+        }
+
+        /// <summary>
+        /// 根据data得到挑选 dataserver
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        IPipelineRef getServer(byte[] data)
+        {
+            int hash = Helper_NEO.CalcHash256(data).GetHashCode() % serverPath.Count;
+            var path = serverPath[hash];
+            return this.DataServerDic[path];
+        }
+    }
+
+    
+}
