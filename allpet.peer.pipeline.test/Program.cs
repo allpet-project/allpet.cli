92c7874cff80f2bcb5309676bd05f9c3ddec5ded
lights <lightsever@hotmail.com>
2019-01-23 15:31:02 +0800

    update test

--- allpet.peer.pipeline.test/Program.cs
+++ allpet.peer.pipeline.test/Program.cs
@@ -37,10 +37,14 @@ namespace AllPet.Pipeline.test
             systemR.OpenNetwork(new AllPet.peer.tcp.PeerOption());
             systemR.OpenListen(new System.Net.IPEndPoint(System.Net.IPAddress.Any, 8888));
             systemR.RegistPipeline("hello", new Hello(systemR));
+            systemR.RegistPipeline("hello2", new Hello(systemR));
+            systemR.Start();
 
 
             var systemL = AllPet.Pipeline.PipelineSystem.CreatePipelineSystemV1();
             systemL.OpenNetwork(new AllPet.peer.tcp.PeerOption());
+            systemL.Start();
+
             var remote = new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 8888);
             var systemref = await systemL.Connect(remote);
             var actor = systemL.GetPipeline(null, "127.0.0.1:8888/hello");
@@ -89,33 +93,36 @@ namespace AllPet.Pipeline.test
         }
     }
 
-    class Hello : Pipeline
+    class Hello : Module
     {
-        public Hello(IPipelineSystem system) : base(system)
+        public Hello(ISystem system) : base(system,false)
         {
         }
-        IModuleRef refhello2;
+        IModulePipeline refhello2;
         public override void OnStart()
         {
             var refhello2 = this.GetPipeline("this/hello2");
-            refhello2.Tell(System.Text.Encoding.UTF8.GetBytes("abcde"));
+            refhello2.Tell(global::System.Text.Encoding.UTF8.GetBytes("abcde"));
         }
-        public override void OnTell(IModuleRef from, byte[] data)
+        public override void OnTell(IModulePipeline from, byte[] data)
         {
-            Console.WriteLine("Hello:" + System.Text.Encoding.UTF8.GetString(data));
+            Console.WriteLine("Hello:" + global::System.Text.Encoding.UTF8.GetString(data));
         }
     }
-    class Hello2 : Pipeline
+    class Hello2 : Module
     {
-        public Hello2(IPipelineSystem system) : base(system)
+        public Hello2(ISystem system) : base(system,false)
         {
 
         }
-        public override void OnTell(IModuleRef from, byte[] data)
+        public override void OnStart()
         {
-            Console.WriteLine("Hello2:" + System.Text.Encoding.UTF8.GetString(data));
+        }
+        public override void OnTell(IModulePipeline from, byte[] data)
+        {
+            Console.WriteLine("Hello2:" + global::System.Text.Encoding.UTF8.GetString(data));
 
-            from.Tell(System.Text.Encoding.UTF8.GetBytes("hello back."));
+            from.Tell(global::System.Text.Encoding.UTF8.GetBytes("hello back."));
         }
     }
 }
