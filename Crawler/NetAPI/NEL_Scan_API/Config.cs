a77faab98e83e06207e58a0247cd1fa83aa7988c
dongbo <895188445@qq,com>
2019-02-04 15:05:27 +0800

    爬虫项目

---
+++ Crawler/NetAPI/NEL_Scan_API/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace NetAPI
+{
+    class Config
+    {
+        public static string mongodbConnStr = string.Empty;
+        public static string mongodbDatabase = string.Empty;
+        public static string NeoCliJsonRPCUrl = string.Empty;
+        public static int sleepTime = 0;
+        public static bool beUtxoSleep = false;
+        public static bool startMonitorFlag = true;
+
+
+        /// <summary>
+        /// 加载配置
+        /// </summary>
+        public static void loadFromPath(string path)
+        {
+            string result = File.ReadAllText(path);
+            MyJson.JsonNode_Object config = MyJson.Parse(result) as MyJson.JsonNode_Object;
+            mongodbConnStr = config.AsDict().GetDictItem("mongodbConnStr").AsString();
+            mongodbDatabase = config.AsDict().GetDictItem("mongodbDatabase").AsString();
+            NeoCliJsonRPCUrl = config.AsDict().GetDictItem("NeoCliJsonRPCUrl").AsString();
+            startMonitorFlag = config.AsDict().GetDictItem("startMonitorFlag").AsBool();
+
+            sleepTime = config.AsDict().GetDictItem("sleepTime").AsInt();
+        }
+
+    }
+}
